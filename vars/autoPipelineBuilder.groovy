65df927d740f763a8355c7252d39334460a30dbe
root <root@ip-172-31-29-118.ec2.internal>
2026-03-06 18:31:07 +0000

    Add auto detection pipeline

---
+++ vars/autoPipelineBuilder.groovy
@@ -0,0 +1,76 @@
+def call() {
+
+    pipeline {
+
+        agent any
+
+        stages {
+
+            stage('Checkout') {
+                steps {
+                    git url: 'https://github.com/dkasha14/jenkins-shared-library.git', branch: 'master'
+                }
+            }
+
+            stage('Detect Application Type') {
+                steps {
+                    script {
+
+                        if (fileExists('requirements.txt')) {
+                            env.APP_TYPE = "python"
+                        }
+                        else if (fileExists('pom.xml')) {
+                            env.APP_TYPE = "java"
+                        }
+                        else if (fileExists('package.json')) {
+                            env.APP_TYPE = "node"
+                        }
+                        else {
+                            env.APP_TYPE = "unknown"
+                        }
+
+                        echo "Detected application type: ${env.APP_TYPE}"
+
+                    }
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    script {
+
+                        if (env.APP_TYPE == "python") {
+                            sh "pip3 install -r requirements.txt"
+                        }
+
+                        if (env.APP_TYPE == "java") {
+                            sh "mvn clean package"
+                        }
+
+                        if (env.APP_TYPE == "node") {
+                            sh "npm install"
+                        }
+
+                        if (env.APP_TYPE == "unknown") {
+                            echo "No supported build file found"
+                        }
+
+                    }
+                }
+            }
+
+            stage('Test') {
+                steps {
+                    echo "Running tests..."
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    echo "Deploy stage..."
+                }
+            }
+
+        }
+    }
+}
